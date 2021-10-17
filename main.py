daed7298dad15494fa1bbc0b3d3a96215ab3aeaa
fruttz <dito.arjwasa@gmail.com>
2021-10-17 21:22:09 +0700

    still unauthorized

--- main.py
+++ main.py
@@ -77,6 +77,8 @@ def create_access_token(data: dict, expires: Optional[timedelta] = None):
     else:
         expire = datetime.utcnow() + timedelta(minutes=15)
     to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 async def get_current_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
